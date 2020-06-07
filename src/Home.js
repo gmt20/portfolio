ab8b8be059deb7cea4e1c5e0787727d4284ba09c
gmt20 <thukralmegha20@gmail.com>
2020-06-07 21:38:45 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+ 
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>HELLO</h2>
+        <p>My name is Megha. I am a Computer Engineer.</p>
+ 
+      </div>
+    );
+  }
+}
+ 
+export default Home;
---
+++ src/Projects.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+ 
+class Projects extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Data Science projects</h2>
+        <p>Here is the list of some of the data science projects that I have done:</p>
+        <ol>
+          <li>Stack Overflow Chat Bot</li>
+          <li>Image Captioning Porject</li>
+          <li>Text Classifiation</li>
+          <li>Named Entity Recognition</li>
+          
+        </ol>
+        <h2>Web development projects</h2>
+        <p>Here is the list of some of the web developments projects that I have done:</p>
+        <ol>
+          <li>Project Tracker App in pure Javascript</li>
+          <li>Portfolio Web app in Reacts js</li>
+          <li>Save and define chrome extension in Javascript</li>
+          <li>Node js App</li>
+          <li>Full MERN Stack app</li>
+                    
+        </ol>
+        <h2> Basic development projects</h2>
+        <p>Here is the list of some of the web developments projects that I have done:</p>
+        <ol>
+          <li>Web Scrapping in Python using beautiful soap</li>
+          <li>Interpreter/Compiler</li>
+          <li>Chess Engine</li>
+          <li>Micro kernel</li>
+          <li>HTTP Proxy Server</li>
+                    
+        </ol>
+      </div>
+    );
+  }
+}
+ 
+export default Projects;
