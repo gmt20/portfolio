d8c7d68402682cf3dbeafb66762e7cca1332f3a0
gmt20 <thukralmegha20@gmail.com>
2020-06-07 22:40:52 +0530

    Updates

---
+++ precache-manifest.012a6718d3330b08c5432b24704e4f98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e64ef714261dbd63fcce05984bb2e19",
+    "url": "/gmt20/portfolio/index.html"
+  },
+  {
+    "revision": "21eb9d03c7d46783de03",
+    "url": "/gmt20/portfolio/static/css/main.f6bd4e0c.chunk.css"
+  },
+  {
+    "revision": "0eb160b0599cde266bc2",
+    "url": "/gmt20/portfolio/static/js/2.713e1ce2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gmt20/portfolio/static/js/2.713e1ce2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21eb9d03c7d46783de03",
+    "url": "/gmt20/portfolio/static/js/main.3f850653.chunk.js"
+  },
+  {
+    "revision": "fedafbd2c9f862da0091",
+    "url": "/gmt20/portfolio/static/js/runtime-main.1bc5ec4e.js"
+  }
+]);
